63a0159b7be8d9ecc9d1890495102d11201c4728
o_thollas <o_thollas@hotmail.com>
2025-01-22 00:19:39 +0100

    Ajout de fonctions pour paramétrer le mot dans le jeu du pendu.

--- main.py
+++ main.py
@@ -14,11 +14,20 @@ print("""
 """)
 
 
-print(w)
+
 # Definir une fonction pour choisir un mot au hasard dans le dictionnaire ou la liste presente dans words.py ou fichier json
 
+def get_word() :
+    index = (math.ceil(random.random()*len(w)-1))
+    selected_word = w[index]
+    return selected_word
 # Prendre le mot et le changer en dictionnaire ou en  tulpe grace au set, pour attribuer la valeur false à chaque lettre du mot
-
+def get_word_for_game() :
+    word = get_word()
+    word = word.lower()
+    defragmented_word = [(letter, False) for letter in word]
+    defragmented_word = set(defragmented_word)
+    return defragmented_word
 # Initialiser le mot avec des _ si la lettre est à false
 
 # Faire une entrer utilisateur 
@@ -28,3 +37,5 @@ print(w)
 # L'utilisateur auras 6 essais et à chaque essais rater le pendu sera construit
 
 # si l'utilsateur à deviner le mot avant la fin de la construction du pendu alors il 
+
+print(get_word_for_game())
